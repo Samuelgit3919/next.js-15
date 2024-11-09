c3be040ab9703806c6c9c471fa5c218aed654ff6
samuel sami <saminew3919@gmail.com>
2024-11-08 21:51:22 -0800

    commmit

---
+++ next.js-15/app/api/books/route.ts
@@ -0,0 +1,6 @@
+import books from "@/app/api/db";
+
+
+export async function GET() {
+    return Response.json(books)
+}
---
+++ next.js-15/app/api/db.ts
@@ -0,0 +1,8 @@
+const books = [
+    { id: 1, name: "Atomic Habit" },
+    { id: 2, name: "Deep Work" },
+    { id: 3, name: "The 7 Habit of Highly Effective People" }
+
+];
+
+export default books;
